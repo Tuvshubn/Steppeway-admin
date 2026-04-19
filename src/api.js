9f9f2fe02e0b47704bdad20179b17d9125492d9b
tuvshu <tuvshin-ochir.g@simple.mn>
2026-04-20 06:09:44 +0800

    dd

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE = import.meta.env.VITE_API_URL || 'https://steppeway-back-end.vercel.app/';
+const BASE = import.meta.env.VITE_API_URL || "https://steppeway-back-end.vercel.app/";
 
 const api = axios.create({baseURL: BASE});
 api.interceptors.request.use(cfg => {
